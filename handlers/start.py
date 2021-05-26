c46ad9ec567bfe30feb8bf4c6e921537d7c3c324
Tofik Denianto <77754555+tofikdn@users.noreply.github.com>
2021-05-26 23:18:01 +0700

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -71,24 +71,24 @@ async def start(client: Client, message: Message):
 async def help(client: Client, message: Message):
     await message.reply_text(
         f"""<b>Hi {message.from_user.first_name}!
-\n/play <song name> - play song you requested
-/dplay <song name> - play song you requested via deezer
-/splay <song name> - play song you requested via jio saavn
-/playlist - Show now playing list
-/current - Show now playing
-/song <song name> - download songs you want quickly
-/search <query> - search videos on youtube with details
-/deezer <song name> - download songs you want quickly via deezer
-/saavn <song name> - download songs you want quickly via saavn
-/video <song name> - download videos you want quickly
+\n/play <nama lagu>  - Untuk Memutar lagu yang Anda minta melalui youtube
+/dplay <nama lagu>  - Untuk Memutar lagu yang Anda minta melalui deezer
+/splay <nama lagu>  - Untuk Memutar lagu yang Anda minta melalui jio saavn
+/playlist - Untuk Menampilkan daftar putar Lagu sekarang
+/current - Untuk Menunjukkan  Lagu sekarang yang sedang diputar
+/song <nama lagu> - Untuk Mendownload lagu dari YouTube 
+/search <nama lagu> - Untuk Mencari Video di YouTube dengan detail
+/deezer <nama lagu> - Untuk Mendownload lagu dari deezer 
+/saavn <nama lagu> - Untuk Mendownload lagu dari website saavn
+/video <nama lagu> - Untuk Mendownload Video di YouTube dengan detail
 \n*Admins only*
-/player - open music player settings panel
-/pause - pause song play
-/resume - resume song play
-/skip - play next song
-/end - stop music play
-/userbotjoin - invite assistant to your chat
-/admincache - Refresh admin list
+/player - Open music player settings panel
+/pause - Untuk Menjeda pemutaran Lagu
+/resume - Untuk Melanjutkan pemutaran Lagu yang di pause
+/skip - Untuk Menskip pemutaran lagu ke Lagu berikutnya
+/end - Untuk Memberhentikan pemutaran Lagu
+/userbotjoin - Untuk Mengundang asisten ke obrolan Anda
+/admincache - Untuk Merefresh admin list
  </b>""",
         reply_markup=InlineKeyboardMarkup(
             [
