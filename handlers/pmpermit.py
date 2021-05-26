262fa1698537579e571c32c1b62e12399cfd5fb5
Tofik Denianto <77754555+tofikdn@users.noreply.github.com>
2021-05-26 18:03:07 +0700

    Update pmpermit.py

--- handlers/pmpermit.py
+++ handlers/pmpermit.py
@@ -5,5 +5,5 @@ from pyrogram.types import Chat, Message, User
 
 @USER.on_message(filters.text & filters.private & ~filters.me & ~filters.bot)
 async def pmPermit(client: USER, message: Message):
-  await USER.send_message(message.chat.id,"Halo, Saya adalah **Layanan Assistant TDMusicBot.**\n\n ❗️ **Rules:**\n   - Jangan Spam Pesan disini\n   - Jangan Spam Lagu Biar Ga Error \n\n 👉 **KIRIM LINK INVITE ATAU USERNAME GROUP, JIKA ASSISTANT TIDAK DAPAT BERGABUNG DENGAN GROUP ANDA.**\n\n **Owner** @tofik_dn**")
+  await USER.send_message(message.chat.id,"Halo, Saya adalah **Layanan Assistant TD Music Bot.**\n\n ❗️ **Rules:**\n   - Jangan Spam Pesan disini\n   - Jangan Spam Lagu Biar Ga Error \n\n 👉 **KIRIM LINK INVITE ATAU USERNAME GROUP, JIKA ASSISTANT TIDAK DAPAT BERGABUNG DENGAN GROUP ANDA.**\n\n **Owner** @tofik_dn**")
   return                        
