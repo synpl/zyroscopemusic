a41da1b9cff498ee036b65988d92543463363c2e
Tofik Denianto <77754555+tofikdn@users.noreply.github.com>
2021-06-07 15:16:35 +0700

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -104,13 +104,13 @@ async def generate_cover(requested_by, title, views, duration, thumbnail):
     img = Image.open("temp.png")
     draw = ImageDraw.Draw(img)
     font = ImageFont.truetype("etc/font.otf", 32)
-    draw.text((205, 550), f"Title: {title}", (51, 215, 255), font=font)
+    draw.text((205, 550), f"Judul: {title}", (51, 215, 255), font=font)
     draw.text(
-        (205, 590), f"Duration: {duration}", (255, 255, 255), font=font
+        (205, 590), f"Durasi: {duration}", (255, 255, 255), font=font
     )
     draw.text((205, 630), f"Views: {views}", (255, 255, 255), font=font)
     draw.text((205, 670),
-        f"Added By: {requested_by}",
+        f"Atas permintaan: {requested_by}",
         (255, 255, 255),
         font=font,
     )
