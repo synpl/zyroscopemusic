ba4b851952f3e0500ab78b7c3760ae1c80f1eadc
Tofik Denianto <77754555+tofikdn@users.noreply.github.com>
2021-06-27 16:16:44 +0700

    Update config.py

--- config.py
+++ config.py
@@ -12,6 +12,6 @@ API_HASH = getenv("API_HASH")
 
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
 
-COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! . ?").split())
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
