a9d4061d1f5c99ddc7c718e35b3b5ec144fb1901
Tofik Denianto <77754555+tofikdn@users.noreply.github.com>
2021-07-05 19:06:13 +0700

    Update ping.py

--- handlers/ping.py
+++ handlers/ping.py
@@ -3,6 +3,7 @@ from time import time
 
 from pyrogram import Client, filters, emoji
 from pyrogram.types import Message
+from helpers.filters import command
 
 START_TIME = datetime.utcnow()
 START_TIME_ISO = START_TIME.replace(microsecond=0).isoformat()
@@ -33,7 +34,6 @@ async def _human_time_duration(seconds):
     & ~ filters.edited
 )
 async def ping_pong(client: Client, m: Message):
-    """Reply ping with pong and delete both messages"""
     start = time()
     m_reply = await m.reply_text("Pinging...")
     delta_ping = time() - start
@@ -48,7 +48,6 @@ async def ping_pong(client: Client, m: Message):
     & ~ filters.edited
 )
 async def get_uptime(client: Client, m: Message):
-    """/uptime Reply with readable uptime and ISO 8601 start time"""
     current_time = datetime.utcnow()
     uptime_sec = (current_time - START_TIME).total_seconds()
     uptime = await _human_time_duration(int(uptime_sec))
