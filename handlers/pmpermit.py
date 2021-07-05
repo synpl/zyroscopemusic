d7c3b7b566f4f804acc3b2a1a9f2d9717e26a2a2
Tofik Denianto <77754555+tofikdn@users.noreply.github.com>
2021-07-05 19:07:41 +0700

    Delete pmpermit.py

--- handlers/pmpermit.py
+++
@@ -1,9 +0,0 @@
-from callsmusic.callsmusic import client as USER
-from pyrogram import filters
-from pyrogram.types import Chat, Message, User
-
-
-@USER.on_message(filters.text & filters.private & ~filters.me & ~filters.bot)
-async def pmPermit(client: USER, message: Message):
-  await USER.send_message(message.chat.id,"Halo, Saya adalah **Layanan Assistant [TD Music Bot](https://t.me/tofikdnbot).**\n\n ❗️ **Rules:**\n   - Jangan Spam Pesan disini\n   - Jangan Spam Lagu Biar Ga Error \n\n 👉 **KIRIM LINK INVITE ATAU USERNAME GROUP, JIKA ASSISTANT TIDAK DAPAT BERGABUNG DENGAN GROUP ANDA.**")
-  return                        
