bda4ef4fc7ea5317f255079e22bdc985884a11ce
Tofik Denianto <77754555+tofikdn@users.noreply.github.com>
2021-07-12 21:19:40 +0700

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,8 +1,32 @@
-from .ping import *
+from time import time
+from datetime import datetime
 from config import BOT_USERNAME, BOT_NAME, ASSISTANT_NAME
 from helpers.filters import command
-from pyrogram import Client, filters
+from pyrogram import Client, filters, emoji
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+
+START_TIME = datetime.utcnow()
+START_TIME_ISO = START_TIME.replace(microsecond=0).isoformat()
+TIME_DURATION_UNITS = (
+    ('week', 60 * 60 * 24 * 7),
+    ('day', 60 * 60 * 24),
+    ('hour', 60 * 60),
+    ('min', 60),
+    ('sec', 1)
+)
+
+async def _human_time_duration(seconds):
+    if seconds == 0:
+        return 'inf'
+    parts = []
+    for unit, div in TIME_DURATION_UNITS:
+        amount, seconds = divmod(int(seconds), div)
+        if amount > 0:
+            parts.append('{} {}{}'
+                         .format(amount, unit, "" if amount == 1 else "s"))
+    return ', '.join(parts)
+
 
 @Client.on_message(command("start") & filters.private & ~filters.edited)
 async def start_(client: Client, message: Message):
@@ -35,11 +59,9 @@ Ketik » /help « Untuk Melihat Daftar Perintah!
      disable_web_page_preview=False
     )
 
+
 @Client.on_message(command(["start", "start@{BOT_USERNAME}"]) & filters.group & ~filters.edited)
 async def start(client: Client, message: Message):
-    current_time = datetime.utcnow()
-    uptime_sec = (current_time - START_TIME).total_seconds()
-    uptime = await _human_time_duration(int(uptime_sec))
     await message.reply_text(
         f"""I'm online!\n<b>Up since:</b> {uptime}""",
         reply_markup=InlineKeyboardMarkup(
@@ -55,6 +77,7 @@ async def start(client: Client, message: Message):
             ]
         )
     )
+
 
 @Client.on_message(command("help") & filters.private & ~filters.edited)
 async def help(client: Client, message: Message):
@@ -87,4 +110,27 @@ async def help(client: Client, message: Message):
                 ]
             ]
         )
+    )
+
+
+@Client.on_message(command("ping") & ~filters.edited)
+async def ping_pong(client: Client, m: Message):
+    start = time()
+    m_reply = await m.reply_text("Pinging...")
+    delta_ping = time() - start
+    await m_reply.edit_text(
+        f"{emoji.PING_PONG} **PONG!!**\n"
+        f"`{delta_ping * 1000:.3f} ms`"
+    )
+
+
+@Client.on_message(command("uptime") & ~filters.edited)
+async def get_uptime(client: Client, m: Message):
+    current_time = datetime.utcnow()
+    uptime_sec = (current_time - START_TIME).total_seconds()
+    uptime = await _human_time_duration(int(uptime_sec))
+    await m.reply_text(
+        f"{emoji.ROBOT}\n"
+        f"• **Uptime:** `{uptime}`\n"
+        f"• **Start Time:** `{START_TIME_ISO}`"
     )
