2f93701afaba4f4f0ae3d674d020a7bb3008951c
Tofik Denianto <77754555+tofikdn@users.noreply.github.com>
2021-07-30 10:01:51 +0700

    sudoersss

--- helpers/filters.py
+++ helpers/filters.py
@@ -19,10 +19,11 @@ from typing import Union, List
 
 from pyrogram import filters
 
-from config import COMMAND_PREFIXES
+from config import COMMAND_PREFIXES, SUDO_USERS
 
 other_filters = filters.group & ~ filters.edited & ~ filters.via_bot & ~ filters.forwarded
 other_filters2 = filters.private & ~ filters.edited & ~ filters.via_bot & ~ filters.forwarded
+sudo_only = filters.user(SUDO_USERS)
 
 
 def command(commands: Union[str, List[str]]):
