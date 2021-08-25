08113ba0582a29ba2332b511012342dcde6e66b0
Tofik Denianto <77754555+tofikdn@users.noreply.github.com>
2021-08-25 11:20:32 +0700

    Update asupan.py
    new api url

--- handlers/asupan.py
+++ handlers/asupan.py
@@ -9,7 +9,7 @@ from helpers.filters import command
 @Client.on_message(command(["asupan", f"asupan@{BOT_USERNAME}"]))
 async def asupan(client, message):
     try:
-        resp = requests.get("https://tede-api.herokuapp.com/api/asupan/ptl").json()
+        resp = requests.get("https://api-tede.herokuapp.com/api/asupan/ptl").json()
         results = f"{resp['url']}"
         return await client.send_video(message.chat.id, video=results)
     except Exception:
@@ -19,7 +19,7 @@ async def asupan(client, message):
 @Client.on_message(command(["wibu", f"wibu@{BOT_USERNAME}"]))
 async def wibu(client, message):
     try:
-        resp = requests.get("https://tede-api.herokuapp.com/api/asupan/wibu").json()
+        resp = requests.get("https://api-tede.herokuapp.com/api/asupan/wibu").json()
         results = f"{resp['url']}"
         return await client.send_video(message.chat.id, video=results)
     except Exception:
@@ -29,7 +29,7 @@ async def wibu(client, message):
 @Client.on_message(command(["chika", f"chika@{BOT_USERNAME}"]))
 async def chika(client, message):
     try:
-        resp = requests.get("https://tede-api.herokuapp.com/api/chika").json()
+        resp = requests.get("https://api-tede.herokuapp.com/api/chika").json()
         results = f"{resp['url']}"
         return await client.send_video(message.chat.id, video=results)
     except Exception:
