15b32dc8819fe5830a242c83217dc3fb6586f66c
Tofik Denianto <77754555+tofikdn@users.noreply.github.com>
2021-09-08 08:18:11 +0700

    Update userbotjoin.py

--- handlers/userbotjoin.py
+++ handlers/userbotjoin.py
@@ -49,25 +49,27 @@ async def rem(USER, message):
         await USER.leave_chat(message.chat.id)
     except:  
         await message.reply_text(
-            f"<b>Pengguna tidak dapat meninggalkan group Anda! Mungkin menunggu floodwaits."
-            "\n\nAtau keluarkan saya secara manual dari ke Group Anda</b>",
+            '<b>Pengguna tidak dapat meninggalkan group Anda! Mungkin menunggu floodwaits.\n\nAtau keluarkan saya secara manual dari ke Group Anda</b>'
         )
+
         return
 
 
 @Client.on_message(command("userbotleaveall") & ~filters.edited)
 async def bye(client, message):
-    if message.from_user.id in SUDO_USERS:
-        left=0
-        failed=0
-        lol = await message.reply("Assistant Leaving all chats")
-        async for dialog in USER.iter_dialogs():
-            try:
-                await USER.leave_chat(dialog.chat.id)
-                left = left+1
-                await lol.edit(f"Assistant leaving... Left: {left} chats. Failed: {failed} chats.")
-            except:
-                failed=failed+1
-                await lol.edit(f"Assistant leaving... Left: {left} chats. Failed: {failed} chats.")
-            await asyncio.sleep(0.7)
-        await client.send_message(message.chat.id, f"Left {left} chats. Failed {failed} chats.")
+    if message.from_user.id not in SUDO_USERS:
+        return
+
+    left=0
+    failed=0
+    lol = await message.reply("Assistant Leaving all chats")
+    async for dialog in USER.iter_dialogs():
+        try:
+            await USER.leave_chat(dialog.chat.id)
+            left += 1
+            await lol.edit(f"Assistant leaving... Left: {left} chats. Failed: {failed} chats.")
+        except:
+            failed += 1
+            await lol.edit(f"Assistant leaving... Left: {left} chats. Failed: {failed} chats.")
+        await asyncio.sleep(0.7)
+    await client.send_message(message.chat.id, f"Left {left} chats. Failed {failed} chats.")
