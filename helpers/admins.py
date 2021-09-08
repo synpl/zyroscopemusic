9f22732b2655d3d9ab06f2e85b72c45a17ef7717
Tofik Denianto <77754555+tofikdn@users.noreply.github.com>
2021-09-08 08:20:33 +0700

    Update admins.py

--- helpers/admins.py
+++ helpers/admins.py
@@ -26,13 +26,8 @@ async def get_administrators(chat: Chat) -> List[User]:
 
     if get:
         return get
-    else:
-        administrators = await chat.get_members(filter="administrators")
-        to_set = []
+    administrators = await chat.get_members(filter="administrators")
+    to_set = [administrator.user.id for administrator in administrators]
 
-        for administrator in administrators:
-            #if administrator.can_manage_voice_chats:
-            to_set.append(administrator.user.id)
-
-        cache.admins.set(chat.id, to_set)
-        return await get_administrators(chat)
+    cache.admins.set(chat.id, to_set)
+    return await get_administrators(chat)
